2fd86f001543fdeff35cc9c56b2be10e158addba
dnkrit <dnkrit@yandex.ru>
2024-04-23 23:57:02 +0300

    add new const

--- main.py
+++ main.py
@@ -2,10 +2,15 @@ import pygame
 
 pygame.init()
 
+SCREEN_WIDTH = 800
+SCREEN_WEIGHT = 600
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_WEIGHT))
+
+pygame.display.set_caption('Игра Тип')
+
 running = True
 while running:
     pass
 
  pygame.quit()
 
- 
